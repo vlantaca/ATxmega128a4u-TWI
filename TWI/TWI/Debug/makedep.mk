908d522f17dd43da9da40113d8c4687f81678fae
Vincent <vlantaca@ucsc.edu>
2014-04-07 19:03:24 -0700

    initial commit

---
+++ TWI/TWI/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+TWI.c
+
+twi_master_driver.c
+
+twi_slave_driver.c
+
